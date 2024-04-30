6c87a9578159bb795f5637bd0da5d8cb6984f453
YC酱luyancib <68143180+luyanci@users.noreply.github.com>
2024-04-30 18:27:14 +0800

    Initial commit

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+	lang: 'zh-CN',
+        base: '/',
+	title: 'Hero',
+	description: '基于 vitepress 的文档模板',
+	head: [['link', { rel: 'icon', href: '/favicon.png' }]],
+	themeConfig: {
+		logo: 'logo.svg',
+		footer: {
+			message: 'Released under the MIT License.',
+			copyright: 'Copyright © 2022 markthree'
+		},
+		nav: [
+			{
+				text: 'Guide',
+				link: '/guide/'
+			},
+			{
+				text: 'About',
+				link: '/about/'
+			}
+		],
+		socialLinks: [
+			{
+				icon: 'github',
+				link: 'https://github.com/name/repo'
+			}
+		]
+	}
+})
---
+++ docs/index.md
@@ -0,0 +1,31 @@
+---
+title: home
+layout: home
+sidebar: false
+
+hero:
+  name: Home
+  text: vitepress 的文档模板
+  tagline: 更快的开发开源文档
+  image:
+    src: /logo.svg
+    alt: Refs
+  actions:
+    - theme: brand
+      text: guide
+      link: /guide/
+    - theme: alt
+      text: about
+      link: /about/
+
+features:
+  - title: TODO
+    details: TODO
+    icon: ⚡
+  - title: TODO
+    details: TODO
+    icon: ☁️
+  - title: TODO
+    details: TODO
+    icon: 🚀
+---
